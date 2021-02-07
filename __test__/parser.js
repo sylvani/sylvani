af3ff6eebbf6f07faf250a1ec68bf51054000cbe
ncomplex <num.complex@pm.me>
2021-02-06 22:56:35 -0500

    remove files

--- __test__/parser.js
+++
@@ -1,9 +0,0 @@
-const Parser = require("../src/parser/Parser");
-
-const parser = new Parser();
-
-const program = `255`;
-
-const ast = parser.parse(program);
-
-console.log(JSON.stringify(ast, null, 2));
